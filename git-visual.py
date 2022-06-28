8f89369dac11f2f264f8a99959ad724429e070ff
Sirio Balmelli <sirio@b-ad.ch>
2022-06-28 22:38:17 +0200

    cleanups, export PDFs

--- git-visual.py
+++ git-visual.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 # 2022 Sirio Balmelli
 
-from enum import Enum
-import matplotlib.pyplot as plt
 from datetime import date
 from dateutil.relativedelta import relativedelta
-from subprocess import run
+from enum import Enum
 from os import chdir, environ, getcwd, path
+from subprocess import run
+
+import matplotlib.pyplot as plt
 import re
 
 
@@ -88,8 +89,8 @@ def gather(start: date, period: Period, cwd: str) -> tuple:
                 stat = m.group(1)
 
                 st = stats[author].get(stat, [])
-                st += [(0, 0.0) for j in range(len(st), i)]  # zero-pad leading
-                st.append((int(m.group(2)), int(m.group(3))/100))
+                st += [0 for j in range(len(st), i)]  # zero-pad leading
+                st.append(int(m.group(2)))
 
                 stats[author][stat] = st
 
@@ -98,14 +99,12 @@ def gather(start: date, period: Period, cwd: str) -> tuple:
         for author in stats.keys():
             for stat in re_names:
                 st = stats[author].get(stat, [])
-                stats[author][stat] = st + [(0, 0.0) for j in range(len(st), i)]  # noqa: E501
+                stats[author][stat] = st + [0 for j in range(len(st), i)]
 
     # sort stats dictionary: helps when debugging/printing/graphing
     stats = {auth: {stat: stats[auth][stat]
-                    for stat in sorted(stats[auth].keys())
-                    }
-             for auth in sorted(stats.keys())
-             }
+                    for stat in sorted(stats[auth].keys())}
+             for auth in sorted(stats.keys())}
 
     return (periods, stats, path.basename(cwd))
 
@@ -137,36 +136,41 @@ def gather_merge(gathers: list) -> tuple:
             """
             Merge values for '(auth, stat)'
             """
-            t1 = stats.get(auth, {}).get(stat) or [(0, 0.0) for i in range(len(periods))]  # noqa: E501
-            t2 = s2.get(auth, {}).get(stat) or [(0, 0.0) for i in range(len(periods))]  # noqa: E501
-            return [(t1[i][0] + t2[i][0], (t1[i][1] + t2[i][1]) / 2)
-                    for i in range(len(periods))
-                    ]
+            t1 = stats.get(auth, {}).get(stat) or [0 for i in range(len(periods))]  # noqa: E501
+            t2 = s2.get(auth, {}).get(stat) or [0 for i in range(len(periods))]
+            return [t1[i] + t2[i] for i in range(len(periods))]
 
         stats = {auth: {stat: mval(auth, stat) for stat in mstats(auth)}
-                 for auth in mauths()
-                 }
+                 for auth in mauths()}
 
         label = f'{label}; {l2}'
 
     return periods, stats, label
 
 
+# TODO: refactor plotting function, there is double compute here
 # common variables for all plotting functions
 plot_fmt = {'alpha': 0.7,
             'antialiased': True}
+plot_save = None
+# cargo cult programming at its finest
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
 
 
 def stackplot(periods, stats, label) -> None:
-    y_num = {k: [stats[k]['insertions'][i][0] + stats[k]['deletions'][i][0]
-                 for i in range(len(stats[k]['insertions']))]
-             for k in stats.keys()}
-    y_ins = {k: [stats[k]['insertions'][i][1]
-             for i in range(len(stats[k]['insertions']))]
-             for k in stats.keys()}
-    y_del = {k: [stats[k]['deletions'][i][1]
-             for i in range(len(stats[k]['insertions']))]
-             for k in stats.keys()}
+    authors = stats.keys()
+    length = range(len(periods))
+
+    y_num = {a: [stats[a]['insertions'][i] + stats[a]['deletions'][i]
+                 for i in length]
+             for a in authors}
+    y_ins = {a: [stats[a]['insertions'][i] / sum(stats[k]['insertions'][i] for k in authors)  # noqa: E501
+                 for i in length]
+             for a in authors}
+    y_del = {a: [stats[a]['deletions'][i] / sum(stats[k]['deletions'][i] for k in authors)  # noqa: E501
+                 for i in length]
+             for a in authors}
 
     # plot total changes by author
     fig, ax = plt.subplots(nrows=3, ncols=1, sharex=True)
@@ -190,55 +194,69 @@ def stackplot(periods, stats, label) -> None:
     ax[0].set_title(label)
     ax[1].legend(loc='best')
 
-    plt.show()
+    # TODO: user a proper path library throughout this whole tool
+    if plot_save:
+        plt.draw()
+        fig.savefig(f'{plot_save}/{periods[-1]} - {label} - stackplot.pdf',
+                    bbox_inches='tight', format='pdf')
+    else:
+        plt.show()
 
 
 def barchart(periods, stats, label) -> None:
-    fig, ax = plt.subplots()
-    label_fmt = {'label_type': 'edge',
-                 'fontsize': 'x-small'}
+    authors = stats.keys()
+    length = range(len(periods))
 
-    x = [i for i in range(len(periods))]
-    ax.set_xticks(x)
+    fig, ax = plt.subplots()
+    label_fmt = {'fontsize': 'x-small'}
+
+    ax.set_xticks([n for n in length])
     ax.set_xticklabels(periods)
 
     # Bar placement is the _center_ of the bar, x is the _center_ of all bars:
     # when aligning remove 1 bar-width, corresponding to half
     # on each side of the group.
-    authors = stats.keys()
     width = 1.0 / (len(authors) + 1)  # leave 1 bar of space between groups
     offset = width * (len(authors) - 1) / 2
     i = 0
     for auth in authors:
-        my_x = [n - offset + (width * i) for n in x]
+        x = [n - offset + (width * i) for n in length]
         i += 1
 
-        a_val = [stats[auth]["insertions"][i][0]
-                 for i in range(len(stats[auth]['insertions']))]
-        a_lbl = [f'{int(stats[auth]["insertions"][i][1] * 100)}%'
-                 for i in range(len(stats[auth]['insertions']))]
+        # insertions
+        a_val = [stats[auth]["insertions"][i] for i in length]
+        a_pct = [stats[auth]['insertions'][i] / sum(stats[k]['insertions'][i] for k in authors)  # noqa: E501
+                 for i in length]
 
         # deletions stacked above insertions, all red, only 1 legend for all
-        one_lbl = '% deletions' if i == 1 else ''  # only label red column once
-        b_val = [stats[auth]["deletions"][i][0]
-                 for i in range(len(stats[auth]['deletions']))]
-        b_lbl = [  # top of deletions column should be total %
-            f'{int((stats[auth]["insertions"][i][1] + stats[auth]["deletions"][i][1]) * 50)}%'  # noqa: E501
-            for i in range(len(stats[auth]['insertions']))]
-        b = ax.bar(my_x, b_val, width=width, label=one_lbl, color='red',
+        one_lbl = 'deletions' if i == 1 else ''  # only label red column once
+        b_val = [stats[auth]["deletions"][i] for i in length]
+        b_pct = [((stats[auth]['deletions'][i]
+                  / sum(stats[k]['deletions'][i] for k in authors)
+                  + a_pct[i]) / 2)
+                 for i in length]
+        b = ax.bar(x, b_val, width=width, label=one_lbl, color='red',
                    bottom=a_val, **plot_fmt)
-        ax.bar_label(b, labels=b_lbl, padding=4, **label_fmt)
+        ax.bar_label(b, labels=[f'{int(p * 100)}%' for p in b_pct],
+                     padding=4, label_type='edge', **label_fmt)
 
         # build 'a' _after_ 'b' so the deletion legend is shown first :P
-        a = ax.bar(my_x, a_val, width=width, label=auth, **plot_fmt)
-        ax.bar_label(a, labels=a_lbl, padding=0, **label_fmt)
+        a = ax.bar(x, a_val, width=width, label=auth, **plot_fmt)
+        ax.bar_label(a, labels=[f'{int(p * 100)}%' for p in a_pct],
+                     padding=0, label_type='center', **label_fmt)  # noqa: E501
 
     ax.set_title(label)
     ax.set_ylabel('Total Lines of Code')
     ax.legend(loc='best')
     ax.margins(y=0.20)  # more space at the top helps legibility
 
-    plt.show()
+    # TODO: user a proper path library throughout this whole tool
+    if plot_save:
+        plt.draw()
+        fig.savefig(f'{plot_save}/{periods[-1]} - {label} - barchart.pdf',
+                    bbox_inches='tight', format='pdf')
+    else:
+        plt.show()
 
 
 if __name__ == "__main__":
@@ -260,13 +278,15 @@ if __name__ == "__main__":
         help=('Start date in ISO (YYYY-mm-dd) format.'
               ' Actual start date will be rounded to the previous Period.')
         )
+    # TODO: add optional 'End'
+
     ap.add_argument(
-        '--paths', metavar='PATH',
+        '-p', '--paths', metavar='PATH',
         nargs='+', dest='paths', type=str,
         help=('Operate on repo at PATH instead of current dir.')
         )
     ap.add_argument(
-        '--label', metavar='LABEL',
+        '-l', '--label', metavar='LABEL',
         dest='label', type=str,
         help=('Text label to put on graphs.')
         )
@@ -274,13 +294,20 @@ if __name__ == "__main__":
                     help=('Generate graph(s) for each individual repo,'
                           ' in addition to the total graph.')
                     )
+    ap.add_argument('-w', '--write', metavar='PATH',
+                    dest='write', type=str,
+                    help=('Write graphs as PDFs to PATH')
+                    )
 
     args = ap.parse_args()
 
+    # TODO: clean up: use argparse defaults
     if args.paths:
         paths = [path.abspath(p) for p in args.paths]
     else:
         paths = [getcwd()]
+    if args.write:
+        plot_save = args.write
 
     # TODO: parallelism (2022-06-28, Sirio Balmelli) #
     each = [gather(args.Start[0], Period[args.Period[0]], p) for p in paths]
@@ -296,5 +323,5 @@ if __name__ == "__main__":
     else:
         label = f'repo(s): {label}'
     # TODO: add a command-line switch (2022-06-28, Sirio Balmelli) #
-    # stackplot(periods, stats, label)
+    stackplot(periods, stats, label)
     barchart(periods, stats, label)
